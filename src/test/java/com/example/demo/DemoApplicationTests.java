1536db1d71ba5f712352a8020035bcee7e5f02d4
Thomas <thomas.anastacio@gmail.com>
2020-07-23 16:00:14 +0200

    Test thing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,30 @@
 package com.example.demo;
 
+import com.example.demo.Model.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Autowired
+    JdbcTemplate test;
 
     @Test
     void contextLoads ()
     {
     }
 
+
+    @Test
+    void dbConnection() {
+        String hola = "SELECT * FROM h2h.user";
+        RowMapper<User> row = new BeanPropertyRowMapper<>(User.class);
+        User yo = test.query(hola, row).get(0);
+        System.out.println(yo.getName());
+
+    }
 }
