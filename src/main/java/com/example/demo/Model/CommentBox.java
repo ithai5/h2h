a268d616fb36512147b40024bd7f0502952d274d
Thomas <thomas.anastacio@gmail.com>
2020-08-05 13:08:34 +0200

    CommentBox model class

---
+++ src/main/java/com/example/demo/Model/CommentBox.java
@@ -0,0 +1,64 @@
+package com.example.demo.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CommentBox {
+    @Id
+    private int commentBoxId;
+    private boolean canComment;
+    private String email;
+    private int listingId;
+
+    public CommentBox() {}
+
+    public CommentBox(int commentBoxId, boolean canComment, String email, int listingId) {
+        this.commentBoxId = commentBoxId;
+        this.canComment = canComment;
+        this.email = email;
+        this.listingId = listingId;
+    }
+
+    public int getCommentBoxId() {
+        return commentBoxId;
+    }
+
+    public void setCommentBoxId(int commentBoxId) {
+        this.commentBoxId = commentBoxId;
+    }
+
+    public boolean isCanComment() {
+        return canComment;
+    }
+
+    public void setCanComment(boolean canComment) {
+        this.canComment = canComment;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getListingId() {
+        return listingId;
+    }
+
+    public void setListingId(int listingId) {
+        this.listingId = listingId;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentBox{" +
+                "commentBoxId=" + commentBoxId +
+                ", canComment=" + canComment +
+                ", email='" + email + '\'' +
+                ", listingId=" + listingId +
+                '}';
+    }
+}
