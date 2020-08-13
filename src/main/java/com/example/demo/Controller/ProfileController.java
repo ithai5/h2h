049df5177654a31caa8a31cb240832de0164503c
Thomas <thomas.anastacio@gmail.com>
2020-08-13 15:32:27 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/com/example/demo/Controller/ProfileController.java
+++ src/main/java/com/example/demo/Controller/ProfileController.java
@@ -1,6 +1,5 @@
 package com.example.demo.Controller;
 
-import com.example.demo.Model.User;
 import com.example.demo.Service.CommentService;
 import com.example.demo.Service.ListingService;
 import com.example.demo.Service.UserService;
@@ -8,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
@@ -15,13 +16,22 @@ public class ProfileController {
     ListingService lService;
     @Autowired
     CommentService cService;
+    @Autowired
+    UserService uService;
 
     @GetMapping("/profile")
     public String currentUserProfile(Model model) {
-        System.out.println(UserController.loginInfo);
         model.addAttribute("currentUser", UserController.loginInfo);
         model.addAttribute("listings", lService.viewUserListing(UserController.loginInfo.getEmail()));
         model.addAttribute("comments", cService.showCommentForBox(cService.fetchBoxForUser(UserController.loginInfo.getEmail()).get(0).getCommentBoxId()));
+        return "/user/currentUserProfile";
+    }
+
+    @PostMapping ("/profile/{email}")
+    public String viewUserProfile(@PathVariable("email") String email, Model model) {
+        model.addAttribute("user", uService.fetchUserByEmail(email));
+        model.addAttribute("listings", lService.viewUserListing(email));
+        model.addAttribute("comments", cService.showCommentForBox(cService.fetchBoxForUser(email).get(0).getCommentBoxId()));
         return "/user/currentUserProfile";
     }
 
